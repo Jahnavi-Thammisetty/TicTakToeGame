ceabedb726bf01311384a8598d3d5d08bccd146b
Jahnavi-Thammisetty <janu.t2001@gmail.com>
2023-07-18 14:05:46 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,60 @@
+console.log("welcome to Tic Tac Toe")
+let music=new Audio("")
+let Audioturn=new Audio("")
+//let gamever=new Audio("")
+let turn="X";
+let gameover=false
+//function to change the turn
+const changeturn = () =>{
+    return turn === "X" ? "0":"X";
+}
+//function to check for a win 
+const checkWin=()=>
+{
+    let boxtext=document.getElementsByClassName("boxtext");
+let wins=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],]
+    wins.forEach(e=>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !=='')){
+            document.querySelector(".info").innerText = boxtext[e[0]].innerText + " won "
+            gameover=true;
+            document.querySelector(".ingbox").getElementsByTagName('img')[0].style.width = "200px";
+        }
+    })
+}
+//game logic
+let boxes=document.getElementsByClassName("box");
+Array.from(boxes).forEach(element=>{
+    let boxtext=element.querySelector(".boxtext");
+    element.addEventListener('click',()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText  = turn;
+             turn=changeturn();
+            Audioturn.play();
+            checkWin();
+            if(!gameover){
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
+            
+        }
+    })
+})
+// add conflict to teset button
+reset .addEventListener('click',()=>{
+     let boxtexts=document.querySelectorAll(".boxtext");
+     Array.from(boxtexts).forEach(element=>{
+        element.innerText = ""
+     });
+     turn="X";
+     gameover=false
+     document.getElementsByClassName("info")[0].innerText = "Turn for" + turn;
+     document.querySelector(".ingbox").getElementsByTagName('img')[0].style.width = "0px";
+
+})
